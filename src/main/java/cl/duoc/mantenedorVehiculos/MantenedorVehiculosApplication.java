34a8c7710ac8e14089e7d53b613872e952752efa
Hanrol <be.cubillosp@duocuc.cl>
2026-04-27 10:56:41 -0400

    primer commit

---
+++ src/main/java/cl/duoc/mantenedorVehiculos/MantenedorVehiculosApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.mantenedorVehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MantenedorVehiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MantenedorVehiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/mantenedorVehiculos/MantenedorVehiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.mantenedorVehiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MantenedorVehiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
