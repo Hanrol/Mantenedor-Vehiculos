ee8db0d3979e5d99dfc527671fa1dee6965d749f
Hanrol <be.cubillosp@duocuc.cl>
2026-04-27 11:27:04 -0400

    ApiResponse creado

---
+++ src/main/java/cl/duoc/mantenedorVehiculos/dto/ApiResponse.java
@@ -0,0 +1,14 @@
+package cl.duoc.mantenedorVehiculos.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+    private int code;
+    private String message;
+    private T data;
+
+}
